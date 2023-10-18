3b9e9de3c989ccc1f1b7edd5d8b51da552053e78
sandrew338 <andrey.nazarkevych@gmail.com>
2023-10-18 20:54:48 +0300

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,116 @@
+// Lab_05_3.cpp
+// < Назаркевич, Андрій>
+// Лабораторна робота № 5.3
+// ряд Тейлера
+// Варіант 17 
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+void S(const double x, const double eps, int& n, double& s);
+void A(const double x, const int n, double& a);
+int main()
+{
+	double xp, xk, x, dx, eps, s = 0;
+
+	int n = 0;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "eps = "; cin >> eps;
+	cout << fixed;
+	cout << "-------------------------------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(10) << "log(x)" << " |"
+		<< setw(7) << "S" << " |"
+		<< setw(5) << "n" << " |"
+		<< endl;
+	cout << "-------------------------------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		S(x, eps, n, s);
+		cout << "|" << setw(7) << setprecision(2) << x << " |"
+			<< setw(10) << setprecision(5) << log(x) << " |"
+			<< setw(10) << setprecision(5) << s << " |"
+			<< setw(5) << n << " |"
+			<< endl;
+		x += dx;
+	}
+	cout << "-------------------------------------------------" << endl;
+	return 0;
+}
+void S(const double x, const double eps, int& n, double& s)
+{
+	n = 0; // вираз залежить від умови завдання варіанту
+	double a = (x - 1) / x; // вираз залежить від умови завдання варіанту
+	s = a;
+	do {
+		n++;
+		A(x, n, a);
+		s += a;
+	} while (abs(a) >= eps);
+}
+void A(const double x, const int n, double& a)
+{
+	double R = 1.0 * (x - 1) * 1.0 * n / (1.0 * (n + 1) * x); // вираз залежить від умови завдання варіанту
+	a *= R;
+}
+/*
+//2 - й спосіб : double - функції
+// Лабораторна робота № 5.2
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+double S(const double x, const double eps, int& n, double s);
+double A(const double x, const int n, double a);
+int main()
+{
+	double xp, xk, x, dx, eps, s = 0;
+	int n = 0;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "eps = "; cin >> eps;
+	cout << fixed;
+	cout << "-------------------------------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(10) << "log(x)" << " |"
+		<< setw(7) << "S" << " |"
+		<< setw(5) << "n" << " |"
+		<< endl;
+	cout << "-------------------------------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		s = S(x, eps, n, s);
+		cout << "|" << setw(7) << setprecision(2) << x << " |"
+			<< setw(10) << setprecision(5) << log(x) << " |"
+			<< setw(10) << setprecision(5) << s << " |"
+			<< setw(5) << n << " |"
+			<< endl;
+		x += dx;
+	}
+	cout << "-------------------------------------------------" << endl;
+	return 0;
+}
+double S(const double x, const double eps, int& n, double s)
+{
+	n = 0; // вираз залежить від умови завдання варіанту
+	double a = (x - 1) / x; // вираз залежить від умови завдання варіанту
+	s = a;
+	do {
+		n++;
+			a = A(x, n, a);
+		s += a;
+	} while (abs(a) >= eps);
+	return s;
+}
+double A(const double x, const int n, double a)
+{
+	double R = 1.0 * (x - 1) * 1.0 * n / (1.0 * (n + 1) * x); // вираз залежить від умови завдання варіанту
+	a *= R;
+	return a;
+}
+*/
